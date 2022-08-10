f3cbd3e2935790b04c69c1cd308d567b38de4d15
chunderella <sijy19@naver.com>
2022-08-10 17:31:00 +0900

    study

--- java-lang/app/src/main/java/com/eomcs/basic/ex02/Exam0212.java
+++ java-lang/app/src/main/java/com/eomcs/basic/ex02/Exam0212.java
@@ -8,20 +8,20 @@ public class Exam0212 {
     Double obj2 = Double.valueOf(3.14);
     Boolean obj3 = Boolean.valueOf(true);
 
-    long l = obj1.LongValue(); //객체로 만든 것을 원래 primitive 값으로 꺼내는 것.
+    long l = obj1.longValue();
     double d = obj2.doubleValue();
     boolean bool = obj3.booleanValue();
 
     // 문자열로 꺼내기
-    //WRAPPER 클래스도 스트랭 클래스 처럼 오브젝트의 투스트링을 () 오버라이딩했다.
+    // Wrapper 클래스도 String 클래스처럼 Object의 toString()을 오버라이딩 했다.
     String s1 = obj1.toString(); // Object의 toString()이 아니다. Long에서 오버라이딩한 toString()이다.
     String s2 = obj2.toString(); // Object의 toString()이 아니다. Double에서 오버라이딩한 toString()이다.
     String s3 = obj3.toString(); // Object의 toString()이 아니다. Boolean에서 오버라이딩한 toString()이다.
 
     // 다른 진수로 변환하여 문자열로 꺼내기
-    System.out.println(Integer.toHexString(i));
-    System.out.println(Integer.toOctalString(i));
-    System.out.println(Integer.toBinaryString(i));
+    System.out.println(Long.toHexString(l));
+    System.out.println(Long.toOctalString(l));
+    System.out.println(Long.toBinaryString(l));
   }
 }
 
