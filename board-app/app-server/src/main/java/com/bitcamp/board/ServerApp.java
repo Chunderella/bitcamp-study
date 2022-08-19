3e0767b9a42c06d246c407dd1cc7f3bcf8d25688
chunderella <sijy19@naver.com>
2022-08-19 16:42:05 +0900

    study:

--- board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
@@ -26,28 +26,28 @@ public class ServerApp {
       servletMap.put("daily", new BoardServlet("daily"));
       servletMap.put("member", new MemberServlet("member"));
 
-      while (true) {
-        try (Socket socket = serverSocket.accept();
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
 
-          System.out.println("클라이언트와 연결 되었음!");
+      try (Socket socket = serverSocket.accept();
+          DataInputStream in = new DataInputStream(socket.getInputStream());
+          DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
 
-          while (true) {
-            // 클라이언트와 서버 사이에 정해진 규칙(protocol)에 따라 데이터를 주고 받는다.
-            String dataName = in.readUTF();
+        System.out.println("클라이언트와 연결 되었음!");
 
-            if (dataName.equals("exit")) {
-              break;
-            }
+        while (true) {
+          // 클라이언트와 서버 사이에 정해진 규칙(protocol)에 따라 데이터를 주고 받는다.
+          String dataName = in.readUTF();
 
-            Servlet servlet = servletMap.get(dataName);
-            if (servlet != null) {
-              servlet.service(in, out);
-            } else {
-              out.writeUTF("fail");
-            }
-          } 
+          if (dataName.equals("exit")) {
+            break;
+          }
+
+          Servlet servlet = servletMap.get(dataName);
+          if (servlet != null) {
+            servlet.service(in, out);
+          } else {
+            out.writeUTF("fail");
+          }
+
 
           System.out.println("클라이언트와 연결을 끊었음!");
         } // 안쪽 try
