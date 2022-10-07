740b757c408de4de3cb717dd26d101e821e0c02f
chunderella <sijy19@naver.com>
2022-10-07 15:19:02 +0900

    study

--- board-app/app-server/src/main/java/com/bitcamp/board/listener/ContextLoaderListener.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/listener/ContextLoaderListener.java
@@ -1,8 +1,11 @@
 package com.bitcamp.board.listener;
 
+import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRegistration.Dynamic;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebListener;
 
 import com.bitcamp.board.dao.BoardDao;
@@ -11,11 +14,14 @@ import com.bitcamp.board.dao.MariaDBMemberDao;
 import com.bitcamp.board.dao.MemberDao;
 import com.bitcamp.board.service.DefaultBoardService;
 import com.bitcamp.board.service.DefaultMemberService;
+import com.bitcamp.servlet.DispatcherServlet;
 import com.bitcamp.sql.DataSource;
 import com.bitcamp.transaction.TransactionManager;
 
 // 웹애플리케이션이 시작되었을 때 공유할 자원을 준비시키거나 해제하는 일을 한다.
 //
+
+@MultipartConfig(maxFileSize = 1024 * 1024 * 10) 
 @WebListener
 public class ContextLoaderListener implements ServletContextListener {
     @Override
@@ -37,6 +43,14 @@ public class ContextLoaderListener implements ServletContextListener {
 
             ctx.setAttribute("boardService", new DefaultBoardService(boardDao, txManager));
             ctx.setAttribute("memberService", new DefaultMemberService(memberDao));
+
+            //자바 코드로 서블릿 객체를 직접 생성하여 서버에 등록하기
+            DispatcherServlet servlet = new DispatcherServlet();
+            Dynamic config = ctx.addServlet("DispatcherServlet", servlet);
+            config.addMapping("/service/*");
+            config.setMultipartConfig(new MultipartConfigElement(
+                    this.getClass().getAnnotation(MultipartConfig.class))); 
+
 
         } catch (Exception e) {
             e.printStackTrace();
