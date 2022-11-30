492125182f0a412b5d60df7913e352997f53cf91
chunderella <sijy19@naver.com>
2022-11-30 23:35:00 +0900

    'quiz'

--- board-app/app-server/src/main/java/com/bitcamp/board/dao/BoardDao.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/dao/BoardDao.java
@@ -2,10 +2,7 @@ package com.bitcamp.board.dao;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
-<<<<<<< HEAD
 import org.apache.ibatis.annotations.Param;
-=======
->>>>>>> 3a7fbaafedd7aa50c6e32bd47bc1a143920b15bd
 import com.bitcamp.board.domain.AttachedFile;
 import com.bitcamp.board.domain.Board;
 
@@ -22,7 +19,6 @@ public interface BoardDao {
 
   int deleteByMember(int memberNo);
 
-<<<<<<< HEAD
   List<Board> findAll(
       @Param("keyword") String keyword, 
       @Param("titleSort") String titleSort,
@@ -30,9 +26,7 @@ public interface BoardDao {
       @Param("size") int size);
 
   int count(@Param("keyword") String keyword, @Param("titleSort") String titleSort);
-=======
-  List<Board> findAll();
->>>>>>> 3a7fbaafedd7aa50c6e32bd47bc1a143920b15bd
+
 
   int insertFiles(Board board);
 
