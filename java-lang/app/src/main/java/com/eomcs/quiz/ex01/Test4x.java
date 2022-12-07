0b800d810c49c7c29f6bfa54cccef77fb700752d
chunderella <sijy19@naver.com>
2022-12-07 17:31:09 +0900

    'q'

--- java-lang/app/src/main/java/com/eomcs/quiz/ex01/Test4x.java
+++ java-lang/app/src/main/java/com/eomcs/quiz/ex01/Test4x.java
@@ -9,7 +9,6 @@ package com.eomcs.quiz.ex01;
 // [시간 복잡도]
 // - ?
 public class Test04 {
-
   public static void main(String[] args) {
     System.out.println(countEvenNumber(1238694636) == 6);
     System.out.println(countEvenNumber(2567884) == 5);
@@ -17,7 +16,13 @@ public class Test04 {
 
   static int countEvenNumber(int value) {
     int result = 0;
-    // 이 메서드를 완성하시오!
+    int n = value;
+    while(n != 0) {
+      if ((n % 2) == 0) {
+        result++;
+      }
+      n /= 10;
+    }
     return result;
   }
 }
